e00309fa2d2f189446474bbf955d22adbdd442d4
rtx-3070 <116584229+rtx-3070@users.noreply.github.com>
2024-02-29 15:23:51 +0100

    Add conditional logic to read keyfile based on environment variable

--- upscale_service.js
+++ upscale_service.js
@@ -3,16 +3,20 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+if (process.env.RENDER === 'true') {
+const keyfile = JSON.parse(fs.readFileSync('/etc/secrets/keys.json', 'utf8'));
+}
+else {
+    const keyfile = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
+}
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-
-app.get('/login', (req, res) => {
-    res.redirect('/');
-});
 
 app.post('/login', (req, res) => {
     const password = req.body.password;
@@ -25,14 +29,10 @@ app.post('/login', (req, res) => {
     }
 });
 
-const keyfile = JSON.parse(fs.readFileSync('keys.json', 'utf8'));
-
 
 function validate_api_key(username, key) {
     return keyfile[username] == key;
 }
-
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
