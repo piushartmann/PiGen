dc888a6e6ed6707da9d3764992a8624018646ef6
rtx-3070 <116584229+rtx-3070@users.noreply.github.com>
2024-02-29 17:38:53 +0100

    express session login

--- upscale_service.js
+++ upscale_service.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+var crypto = require('crypto');
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
 const app = express();
-
-//const keyfile = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
 
 if (fs.existsSync('/etc/secrets/keys.json'))
 {
@@ -16,32 +17,67 @@ else
     global.keyfile = keyfile;
 }
 
+console.log("Starting");
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret: 'pius',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.get('/cookies', (req, res) => {
+    if (Object.keys(req.cookies).length > 0) {
+        res.send('You have cookies!');
+        console.log(req.cookies);
+    } else {
+        res.send('You have no cookies.');
+        console.log(req.cookies);
+    }
+});
+
+app.get('/', (req, res) => {
+    if (req.session.views) {
+      req.session.views++
+    } else {
+      req.session.views = 1
+    }
+    res.render("index.ejs", {views: req.session.views});
+    return;
+  });
+
 
 app.post('/login', (req, res) => {
+    const user = req.body.username;
     const password = req.body.password;
-    const correctPassword = process.env.password;
 
-    if (password === correctPassword) {
+    if (validate_password(user, password)) {
+        res.cookie("id", makeID(user, password), {maxAge: 900000, httpOnly: true});
         res.sendFile(path.join(__dirname, 'private', 'content.html'));
     } else {
         res.redirect('not-allowed.html');
     }
 });
 
-app.get('/testkeyfile', (req, res) => {
-    res.send(keyfile);
-});
+function makeID(user, password) {
+    var hash = crypto.createHash('sha256').update(user+password).digest('hex');
+    return hash
+}
 
-
-function validate_api_key(username, key) {
-    return keyfile[username] == key;
+function validate_password(username, key) {
+    if (!keyfile[username]) {
+        return false;
+    }
+    else {
+        return keyfile[username] == key;
+    }
 }
 
 app.listen(3000, () => {
